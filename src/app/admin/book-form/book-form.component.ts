83967bd9693466f32d46670b88839ca5a0fb522a
The Buildbot <buildbot@angular2buch.de>
2022-07-16 22:57:22 +0000

    Auto-generated commit [ci skip]
    Triggered by commit: https://github.com/angular-schule/buch/commit/94ef049499b3fef816cb57525240dc1243f9a503

---
+++ src/app/admin/book-form/book-form.component.ts
@@ -0,0 +1,24 @@
+import { Component, Output, EventEmitter } from '@angular/core';
+
+import { Book } from '../../shared/book';
+
+@Component({
+  selector: 'bm-book-form',
+  templateUrl: './book-form.component.html',
+  styleUrls: ['./book-form.component.css']
+})
+export class BookFormComponent {
+  book: Book = {
+    isbn: '',
+    title: '',
+    authors: ['']
+  };
+
+  @Output() submitBook = new EventEmitter<Book>();
+
+  constructor() {}
+
+  submitForm() {
+    this.submitBook.emit(this.book);
+  }
+}
