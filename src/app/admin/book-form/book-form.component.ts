67415b59b6f8023ed0f9fb6c3747b37830b4382d
The Buildbot <buildbot@angular2buch.de>
2024-12-09 11:54:29 +0000

    Auto-generated commit
    Triggered by commit: https://github.com/angular-schule/buch/commit/7add1a68068cf75216b0be9059fb8a085799d565

--- src/app/admin/book-form/book-form.component.ts
+++ src/app/admin/book-form/book-form.component.ts
@@ -5,7 +5,8 @@ import { Book } from '../../shared/book';
 @Component({
   selector: 'bm-book-form',
   templateUrl: './book-form.component.html',
-  styleUrls: ['./book-form.component.css']
+  standalone: false,
+  styleUrl: './book-form.component.css'
 })
 export class BookFormComponent {
   book: Book = {
